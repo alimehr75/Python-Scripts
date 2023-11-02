bb7d06a670b4bb86a918b9df76c0a0f9f96cb0ec
Ali Mehraji <a.mehraji75@gmail.com>
2023-11-02 22:18:41 +0330

    Add: Valid yaml script

---
+++ valid_yaml.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python
+import sys
+import yaml
+import json
+
+
+def validator():
+    try:
+        json_data = yaml.load(open(sys.argv[1]).read(), Loader=yaml.FullLoader)
+        json_file = json.dumps(json_data, indent=4)
+        print("Is a Valid Yaml")
+        answer = input("Do You need to see Json Format of your yaml file?[y/n]: ")
+        if answer in ("y","yes","Yes"):
+            print(json_file)
+        else:
+            print("Have good Day")
+
+    except yaml.YAMLError as ex:
+        print("Not Valid File")
+        print(ex)
+    except IndexError:
+        print("""
+        File not found!
+        Usage:
+        python valid_yaml.py Your_Yaml_file.yaml
+        """)
+
+
+validator()
