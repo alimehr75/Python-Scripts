4c0064912578982d200021b632e79738ff4157e2
Ali Mehraji <a.mehraji75@gmail.com>
2024-01-18 18:48:00 +0330

    Add: .gitignore

--- valid_yaml.py
+++ valid_yaml.py
@@ -10,7 +10,7 @@ def validator():
         json_file = json.dumps(json_data, indent=4)
         print("Is a Valid Yaml")
         answer = input("Do You need to see Json Format of your yaml file?[y/n]: ")
-        if answer in ("y","yes","Yes"):
+        if answer in ("y", "yes", "Yes"):
             print(json_file)
         else:
             print("Have good Day")
